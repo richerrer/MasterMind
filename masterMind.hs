7c50fb02285a68033a823980944b12aaa9317772
Raul Mira <mirarodriguezraul0812@gmail.com>
2013-02-03 16:14:24 -0500

    Agrego funciones score first y second part

--- masterMind.hs
+++ masterMind.hs
@@ -59,7 +59,7 @@ new_position actually_pos prohibited_pos 5 = 0
 new_position actually_pos prohibited_pos contador = if not(contador `elem` prohibited_pos) && contador /= actually_pos
                                                      then contador
 													 else new_position actually_pos prohibited_pos (contador+1)
-													 
+
 {-
 Funcion recorrer_lista que recibe como parametros una lista de tuplas que contiene un arreglo y un entero, en nuestro caso
 la lista con todas las combinaciones y tambien una lista,, la cual compara si alguna esta en eela devuelve 1 caso contarrio 0.
@@ -78,3 +78,25 @@ contar_num [] num contador = contador
 contar_num  array num contador = if head array == num
                                   then contar_num(tail array) num (contador+1)
                                   else contar_num (tail array) num (contador)
+{-
+Funcion score_first_part que recibe la lista del codigo, la lista del codigo de la maquina y una tupla que se envia vacia con la intencion de llenar en el
+primer y segundo elemento de la tupla con las listas enviadas anteriormente sin los numeros que coinciden en las mismas posiciones.EJ
+score_first_part1 [1,1,1][1,2,6]([],[]) = ([1,1],[6,2])
+-}
+score_first_part :: [Int]->[Int]->([Int],[Int])->([Int],[Int])
+score_first_part  [] [] array_contenedor = array_contenedor
+score_first_part array1 array2 array_contenedor = if head array1 == head array2
+                                                     then score_first_part (tail array1) (tail array2) array_contenedor
+													 else score_first_part (tail array1) (tail array2) (head array1:fst array_contenedor,head array2:snd array_contenedor)
+{-
+Funcion score_second_part que recibe las listas de los numeros que no coinciden en la misma posicion de la funcion score_first_part y una lista al princio vacia 
+num_enLista que contiene los numeros que coinciden en ambas listas la cual ya sabemos que no estan en la posicion correcta porque esas ya las eliminamos y un contador 
+que dependiendo se suma 1 0 2 debido a que el maximo arreglo es de 4.
+-}
+score_second_part :: [Int]->[Int]->[Int]->Int->Int
+score_second_part [] array2 num_enLista contador = contador
+score_second_part array1 array2 num_enLista contador = if (head array1) `elem` array2 && not((head array1)`elem` num_enLista)
+                                                         then if contar_num array1 (head array1) 0 == contar_num array2 (head array1) 0 && contar_num array1 (head array1) 0 >=2
+														       then score_second_part (tail array1) array2 (head array1:num_enLista) (contador+2)
+															   else score_second_part (tail array1) array2 (head array1:num_enLista) (contador+1)
+                                                         else score_second_part (tail array1) array2 num_enLista contador	
